fc3f78f8e40c8057e9da84fc38e8de735224dc65
李蒙恩 <itcodes@126.com>
2023-04-14 12:42:40 +0800

    First commit

---
+++ AudioClib/Program.cs
@@ -0,0 +1,131 @@
+﻿// var mp3Path = "E:\\Music\\Input\\aaa.mp3";
+// var outputPath = "E:\\Music\\Input\\bbbb.mp3";
+// WavFileUtils.TrimMp3File(mp3Path, outputPath, TimeSpan.FromMinutes(0), TimeSpan.FromMinutes(8));
+// return;
+
+var curDir = Environment.CurrentDirectory;
+var rootPath = curDir.Split("AudioClib\\bin")[0];
+var loadPath = "";
+string inputPath = "Input";
+string outPath = "Export";
+string inputFullPath = rootPath + inputPath;
+string exportFullPath = rootPath + outPath;
+if (!Directory.Exists(inputFullPath))
+{
+    Directory.CreateDirectory(inputFullPath);
+}
+
+if (!Directory.Exists(exportFullPath))
+{
+    Directory.CreateDirectory(exportFullPath);
+}
+
+var GetData = (string filePath) =>
+{
+    string[] lines = System.IO.File.ReadAllLines(filePath);
+    List<AudioData> dataList = new List<AudioData>();
+    for (int i = 0; i < lines.Length; i++)
+    {
+        // 最后一个
+        if (i == lines.Length - 1)
+        {
+            dataList.Add(new AudioData(lines[i],""));
+        }
+        else
+        {
+            dataList.Add(new AudioData(lines[i],lines[i+1]));
+        }
+    }
+
+    return dataList;
+};
+
+
+
+var files = Directory.GetFiles(inputFullPath);
+foreach (var fileInfo in files)
+{
+    FileInfo info = new FileInfo(fileInfo);
+    if (info.Extension == (".txt"))
+    {
+        var fileName = info.Name.Split('.')[0];
+        var audioFile = fileInfo.Split('.')[0] + ".wav";
+        var mp3File = fileInfo.Split('.')[0] + ".mp3";
+        var targetAudioFile = "";
+        if (File.Exists(audioFile))
+        {
+            targetAudioFile = audioFile;
+        }
+        else if (File.Exists(mp3File))
+        {
+            targetAudioFile = mp3File;
+        }
+        else
+        {
+            Console.WriteLine("没有找到音频文件：" + fileName);
+            continue;
+        }
+
+        var singerPath = exportFullPath + "\\" + fileName;
+        Console.WriteLine("开始剪切音频：" + singerPath);
+        if (!Directory.Exists(singerPath))
+        {
+            Directory.CreateDirectory(singerPath);
+        }
+
+        // string jsonStr = File.ReadAllText(fileInfo);
+        // var dataList = JsonConvert.DeserializeObject<List<AudioData>>(jsonStr);
+        var dataList = GetData(fileInfo);
+        if (dataList != null) WavFileUtils.TrimWavListFile(targetAudioFile, singerPath, dataList);
+        // if (dataList != null) WavFileUtils.TrimMp3ListFile(targetAudioFile, singerPath, dataList);
+        
+        
+    }
+}
+
+
+public class AudioData
+{
+    public string Name { get; set; }
+    public int StartTime { get; set; }
+    public int EndTime { get; set; }
+
+    public AudioData(string name, int startTime, int endTime)
+    {
+        Name = name;
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    private int GetTime(string data)
+    {
+        var timeStrArray = data.Split(" ",2);
+        var timeStr = timeStrArray[0];
+        var startTimeArray = timeStr.Split(':');
+        if (startTimeArray.Length == 3) {
+            return int.Parse(startTimeArray[0]) * 3600 + int.Parse(startTimeArray[1]) * 60 + int.Parse(startTimeArray[2]);
+        }
+        return int.Parse(startTimeArray[0])* 60 + int.Parse(startTimeArray[1]);
+    }
+    
+    public AudioData(string curData,string nextStr)
+    {
+        var str = curData.Replace(" - ", " -");
+        str = str.Replace("  ", " ");
+        str = str.Replace("?", "");
+        str = str.Replace("-", "");
+        var strList = str.Split(" ",2);
+        var timeStr = strList[0];
+        Name = strList[1];
+        StartTime = GetTime(curData);
+        if (string.IsNullOrEmpty(nextStr))
+        {
+            EndTime = -1;
+        }
+        else
+        {
+            EndTime = GetTime(nextStr);
+        }
+        
+    }
+}
---
+++ AudioClib/WavFileUtils.cs
@@ -0,0 +1,164 @@
+﻿using NAudio.Wave;
+
+public class WavFileUtils
+{
+    private static List<AudioData> _dataList = new List<AudioData>();
+    /// <summary>
+    /// 基于NAudio工具对Wav音频文件剪切（限PCM格式）
+    /// </summary>
+    /// <param name="audioPath">目标文件</param>
+    /// <param name="outPath">输出文件</param>
+    /// <param name="dataList">截取数据列表</param>
+    public static void TrimWavListFile(string audioPath, string outPath, List<AudioData> dataList)
+    {
+        _dataList = dataList;
+        FileInfo info = new FileInfo(audioPath);
+        var extension = info.Extension;
+
+        if (extension == ".wav")
+        {
+            using (WaveFileReader reader = new WaveFileReader(audioPath))
+            {
+                long fileLength = reader.Length;
+                foreach (var audioData in dataList)
+                {
+                    var songPath = outPath + "\\" + audioData.Name + extension;
+                    using (WaveFileWriter writer = new WaveFileWriter(songPath, reader.WaveFormat))
+                    {
+                        float bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond;
+                        long startPos = (long)Math.Round(audioData.StartTime * bytesPerMillisecond);
+                        startPos -= startPos % reader.WaveFormat.BlockAlign;
+                        long endPos = (long)Math.Round(audioData.EndTime * bytesPerMillisecond);
+                        if (endPos < 0)
+                        {
+                            endPos = fileLength;
+                        }
+                        endPos -= endPos % reader.WaveFormat.BlockAlign;
+                        //判断结束位置是否越界
+                        endPos = endPos > fileLength ? fileLength : endPos;
+                        TrimWavFile(reader, writer, startPos, endPos);
+                        Console.WriteLine($"Export {songPath} Suc！");
+                        break;
+                    }
+                }
+            }
+        }
+        else if (extension == ".mp3")
+        {
+            var reader = new Mp3FileReader(audioPath);
+            foreach (var audioData in _dataList)
+            {
+                var songPath = outPath + "\\" + audioData.Name + ".mp3";
+                if (audioData.EndTime < 0)
+                {
+                    audioData.EndTime = (int)reader.TotalTime.TotalSeconds;
+                }
+                TrimMp3File(reader, songPath, TimeSpan.FromSeconds(audioData.StartTime), TimeSpan.FromSeconds(audioData.EndTime));
+            }
+        }
+
+        Console.WriteLine($"Export All Over!!!");
+    }
+    /// <summary>
+    /// 基于NAudio工具对Wav音频文件剪切（限PCM格式）
+    /// </summary>
+    /// <param name="inPath">目标文件</param>
+    /// <param name="outPath">输出文件</param>
+    /// <param name="cutFromStart">开始时间</param>
+    /// <param name="cutFromEnd">结束时间</param>
+    public static void TrimWavFile(string inPath, string outPath, TimeSpan cutFromStart, TimeSpan cutFromEnd)
+    {
+        using (WaveFileReader reader = new WaveFileReader(inPath))
+        {
+            int fileLength = (int)reader.Length;
+            using (WaveFileWriter writer = new WaveFileWriter(outPath, reader.WaveFormat))
+            {
+                float bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond / 1000f;
+                int startPos = (int)Math.Round(cutFromStart.TotalMilliseconds * bytesPerMillisecond);
+                startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+                int endPos = (int)Math.Round(cutFromEnd.TotalMilliseconds * bytesPerMillisecond);
+                endPos = endPos - endPos % reader.WaveFormat.BlockAlign;
+                //判断结束位置是否越界
+                endPos = endPos > fileLength ? fileLength : endPos;
+                TrimWavFile(reader, writer, startPos, endPos);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 重新合并wav文件
+    /// </summary>
+    /// <param name="reader">读取流</param>
+    /// <param name="writer">写入流</param>
+    /// <param name="startPos">开始流</param>
+    /// <param name="endPos">结束流</param>
+    private static void TrimWavFile(WaveFileReader reader, WaveFileWriter writer, long startPos, long endPos)
+    {
+        reader.Position = startPos;
+        byte[] buffer = new byte[reader.WaveFormat.BlockAlign * 5];
+        while (reader.Position < endPos)
+        {
+            int bytesRequired = (int)(endPos - reader.Position);
+            if (bytesRequired > 0)
+            {
+                int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                if (bytesRead > 0)
+                {
+                    writer.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 基于NAudio工具对MP3音频文件剪切指定片段（补充）
+    /// </summary>
+    /// <param name="inputPath">目标文件路径</param>
+    /// <param name="outputPath">输出文件路径</param>
+    /// <param name="begin">开始时间</param>
+    /// <param name="end">结束时间</param>
+    public static void TrimMp3File(string inputPath, string outputPath, TimeSpan? begin, TimeSpan? end)
+    {
+        if (begin.HasValue && end.HasValue && begin > end)
+            throw new ArgumentOutOfRangeException("end", "end should be greater than begin");
+
+        using (var reader = new Mp3FileReader(inputPath))
+        using (var writer = File.Create(outputPath))
+        {
+            Mp3Frame frame;
+            while ((frame = reader.ReadNextFrame()) != null)
+                if (reader.CurrentTime >= begin || !begin.HasValue)
+                {
+                    if (reader.CurrentTime <= end || !end.HasValue)
+                        writer.Write(frame.RawData, 0, frame.RawData.Length);
+                    else
+                        break;
+                }
+        }
+    }
+    
+    
+    public static void TrimMp3File(Mp3FileReader reader, string outputPath, TimeSpan? begin, TimeSpan? end)
+    {
+        if (begin.HasValue && end.HasValue && begin > end)
+            throw new ArgumentOutOfRangeException("end", "end should be greater than begin");
+
+        using (var writer = File.Create(outputPath))
+        {
+            Mp3Frame frame;
+            while ((frame = reader.ReadNextFrame()) != null)
+                if (reader.CurrentTime >= begin || !begin.HasValue)
+                {
+                    if (reader.CurrentTime <= end || !end.HasValue)
+                        writer.Write(frame.RawData, 0, frame.RawData.Length);
+                    else
+                    {
+                        Console.WriteLine($"Export {outputPath} Suc！");
+                        break; 
+                    }
+                       
+                }
+        }
+    }
+}
